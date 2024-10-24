c7f1446023c9f633973abbc769c3b18aa3650e66
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-10-24 11:58:09 -0300

    realiza as alteracoes

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.getElementById('currency-form').addEventListener('submit', function(event){
     // event.preventDefault(); obter valores de entrada informados élo usuario
-    const valor = document.getElementById('amount')
+    const valor = parseFloa (document.getElementById('fromCurrency').value);
+    const daMoeda = document.getElementById('paraMoeda').value;
 });
