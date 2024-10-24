e3f977eaf8075e02c1fc62c072ee46e6f21e6360
Leonyn <leonyn.rosal@alunoescolassociais.org.br>
2024-10-24 12:17:14 -0300

    realiza as alteracoes

--- main.js
+++ main.js
@@ -2,4 +2,12 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     // event.preventDefault(); obter valores de entrada informados élo usuario
     const valor = parseFloa (document.getElementById('fromCurrency').value);
     const daMoeda = document.getElementById('paraMoeda').value;
+
+    // definir taxas de cambio fixas
+    const exchangeRates = {
+        USD:{BRL:5.70, EUR:0.93},
+        BRL:{USD:0.18, EUR:0.16},
+        EUR:{USD:1.08, BRL:6.16} 
+
+    }
 });
